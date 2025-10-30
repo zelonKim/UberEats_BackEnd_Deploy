3d595a1cc8ad2f250fa059d78e71eb9e79aeded6
zelonKim <ksz18601@gmail.com>
2025-10-30 15:59:14 +0900

    deploy4

--- src/app.module.ts
+++ src/app.module.ts
@@ -73,7 +73,7 @@ import { UploadsModule } from './uploads/uploads.module';
     }),
 
     GraphQLModule.forRoot({
-      //playground: process.env.NODE_ENV !== 'production',
+      playground: true,
       installSubscriptionHandlers: true,
       autoSchemaFile: true,
       context: ({ req, connection }) => {
